be1e859bda614f9a8d1f43309ebd27b0c894ba7e
Salman Rayeen <mohdsr242@gmail.com>
2024-07-22 16:46:07 +0530

    Added increase, decrease quantity of cart item  and delete cart item  feature added for user

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -31,7 +31,7 @@ export const getAddCart = ErrorWrapper(async (req, res, next) => {
         await user.save();
 
         res.status(200).json({
-            message: 'Done',
+            message: 'Requested Food Item added to cart successfully!',
             data: user.cart
         })
         
@@ -39,3 +39,116 @@ export const getAddCart = ErrorWrapper(async (req, res, next) => {
         throw new ErrorHandler(error.statusCode || 500, error.message);
     }
 })
+
+export const getCartItemIncrease = ErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+    let { restaurant_name, category } = req.query;
+
+    try {
+        const restaurant = await Restaurant.findOne({ name: restaurant_name });
+
+        if (!restaurant) {
+        throw new ErrorHandler(401,`Restaurant with name ${restaurant_name} is not exists!`);
+        }
+        
+        const {foodItem} = await restaurant.getFoodItem(category, id);
+
+        const user = await User.findOne({ _id: req.user._id });
+
+        let existingFoodIndex = user.cart.findIndex(item => item.food._id.toString() === foodItem._id.toString());
+
+        if(existingFoodIndex === -1) throw new ErrorHandler(401,`Food Item with id ${id} is not exists in your cart!`);
+        
+        user.cart[existingFoodIndex].quantity ++;
+        
+        await user.save();
+
+        res.status(200).json({
+			message: "Cart Item quantity increased Successfully!",
+			data: user.cart,
+		});
+        
+    } catch (error) {
+        throw new ErrorHandler(error.statusCode || 500, error.message);
+    }
+})
+
+export const getCartItemDecrease = ErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+    let { restaurant_name, category } = req.query;
+
+    try {
+        const restaurant = await Restaurant.findOne({ name: restaurant_name });
+
+        if (!restaurant) {
+        throw new ErrorHandler(401,`Restaurant with name ${restaurant_name} is not exists!`);
+        }
+        
+        const { foodItem } = await restaurant.getFoodItem(category, id);
+        
+        const user = await User.findOne({ _id: req.user._id });
+        
+        let existingFoodIndex = user.cart.findIndex(item => item.food._id.toString() === foodItem._id.toString());
+        
+        if(existingFoodIndex === -1) throw new ErrorHandler(401,`Food Item with id ${id} is not exists in your cart!`);
+        
+        user.cart[existingFoodIndex].quantity--;
+
+        if (user.cart[existingFoodIndex].quantity < 1)
+            user.cart.splice(existingFoodIndex, 1);
+        
+        
+        await user.save();
+
+        res.status(200).json({
+            message: 'Cart Item quantity decreased Successfully!',
+            data: user.cart
+        })
+        
+    } catch (error) {
+        throw new ErrorHandler(error.statusCode || 500, error.message);
+    }
+})
+
+export const getCartItemDelete = ErrorWrapper(async (req, res, next) => {
+	const { id } = req.params;
+	let { restaurant_name, category } = req.query;
+
+	try {
+		const restaurant = await Restaurant.findOne({
+			name: restaurant_name,
+		});
+
+		if (!restaurant) {
+			throw new ErrorHandler(
+				401,
+				`Restaurant with name ${restaurant_name} is not exists!`
+			);
+		}
+
+		const { foodItem } = await restaurant.getFoodItem(category, id);
+
+		const user = await User.findOne({ _id: req.user._id });
+
+		let existingFoodIndex = user.cart.findIndex(
+			(item) => item.food._id.toString() === foodItem._id.toString()
+		);
+
+		if (existingFoodIndex === -1)
+			throw new ErrorHandler(
+				401,
+				`Food Item with id ${id} is not exists in your cart!`
+			);
+
+		user.cart.splice(existingFoodIndex, 1);
+
+		await user.save();
+
+		res.status(200).json({
+			message: "Cart Item deleted Successfully!",
+			data: user.cart,
+		});
+	} catch (error) {
+		throw new ErrorHandler(error.statusCode || 500, error.message);
+	}
+});
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,13 +1,18 @@
 import express from 'express';
-import { getAddCart } from '../controller/user.js';
+import {
+	getAddCart,
+	getCartItemDelete,
+	getCartItemIncrease,
+	getCartItemDecrease,
+} from "../controller/user.js";
 
 const router = express.Router();
 
 
 router.get("/add-cart/:id", getAddCart);
-// router.get("/delete")
-
-
+router.get("/cart-item-increase/:id", getCartItemIncrease);
+router.get("/cart-item-decrease/:id", getCartItemDecrease);
+router.get("/cart-item-delete/:id", getCartItemDelete);
 
 
 
