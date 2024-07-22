14de853c91d1283c98fbe3e8dacc93efd374d412
Salman Rayeen <mohdsr242@gmail.com>
2024-07-22 18:27:44 +0530

    Added post update details feature for user

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -1,6 +1,7 @@
 import User from "../models/user.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
 import ErrorWrapper from "../utils/ErrorWrapper.js";
+import uploadOnCloudinary from "../utils/uploadOnCloudinary.js";
 
 export const getProfile = ErrorWrapper(async (req, res, next) => {
     try {
@@ -11,6 +12,36 @@ export const getProfile = ErrorWrapper(async (req, res, next) => {
             data:user
         })
         
+    } catch (error) {
+        throw new ErrorHandler(error.statusCode || 500, error.message);
+    }
+})
+
+
+
+export const postUpdateDetails = ErrorWrapper(async (req, res, next) => {
+    const { name, username, email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ _id: req.user._id });
+        if (name) user.name = name;
+        if (username) user.username = username;
+        if (email) user.email = email;
+        if (password) user.password = password;
+        
+        let cloudinaryResponse 
+        if (req.file) {
+            cloudinaryResponse = await uploadOnCloudinary(req.file.path)
+            user.image = cloudinaryResponse.url;
+        }
+
+        await user.save();
+        req.user = user;
+
+        res.status(200).json({
+            message: 'User details updated Successfully!',
+            data: user
+        })
     } catch (error) {
         throw new ErrorHandler(error.statusCode || 500, error.message);
     }
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getProfile } from '../controller/user.js';
+import upload from '../utils/multer.js';
+import { getProfile, postUpdateDetails } from "../controller/user.js";
 
 const router = express.Router();
 
 router.get("/get-profile", getProfile);
-
+router.post('/update-details', upload.single('image'), postUpdateDetails);
 
 
 
