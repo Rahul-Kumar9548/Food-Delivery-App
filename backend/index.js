0d76fb36b2f50e3f2721c2d9b1043bc27ecc6e28
Salman Rayeen <mohdsr242@gmail.com>
2024-08-07 16:18:56 +0530

    Checking backend root ki kaam kar rha hai ya nahi isse pahele wala hello world wala kar raha tha

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,8 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "4kb" }));
 app.use(express.static( 'public')); // To store the information that
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
-// app.use('/', authRouter);
+
+app.use('/', authRouter);
 app.use("/cart", verifyjwt, cartRouter);
 app.use("/restaurant", verifyjwt, restaurantRouter);
 app.use('/profile',verifyjwt,userRouter)
