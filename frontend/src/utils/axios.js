da744d31feabcb5e3226c5a8f0ddf381e3aa69aa
Rahul Kumar <rahulkumar290399@gmail.com>
2024-09-24 12:33:31 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 	// baseURL: "http://localhost:4444",
-	baseURL:"https://food-delivery-app-backend-umber.vercel.app",
+	baseURL:"https://food-delivery-app-azure-delta.vercel.app/",
 	withCredentials: true,
 });
 
